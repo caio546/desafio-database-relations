237052a41578685974962ba140d997fcd48afcf0
caio546 <caioom00@gmail.com>
2020-07-30 17:53:41 -0300

    Finish Challenge

--- src/modules/customers/services/CreateCustomerService.ts
+++ src/modules/customers/services/CreateCustomerService.ts
@@ -12,10 +12,21 @@ interface IRequest {
 
 @injectable()
 class CreateCustomerService {
-  constructor(private customersRepository: ICustomersRepository) {}
+  constructor(
+    @inject('CustomersRepository')
+    private customersRepository: ICustomersRepository,
+  ) {}
 
   public async execute({ name, email }: IRequest): Promise<Customer> {
-    // TODO
+    const existentCustomer = await this.customersRepository.findByEmail(email);
+
+    if (existentCustomer) {
+      throw new AppError('Email already exists.');
+    }
+
+    const customer = await this.customersRepository.create({ name, email });
+
+    return customer;
   }
 }
 
